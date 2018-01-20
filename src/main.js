757ff7566be56e5099aa7cf848b7dacdd82624aa
Coder.HanXin <Coder.HanXin@gmail.com>
2018-01-20 19:21:56 +0800

    CzsIcon component

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 import { router } from './router/index'
+import CzsIcon from './views/components/czs-icon'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import './style/index.styl'
 import http from './api'
 
 Vue.config.productionTip = false
+Vue.use(CzsIcon)
 Vue.use(iView)
 Vue.prototype.$http = http
 
---
+++ src/views/components/czs-icon/index.js
@@ -0,0 +1,9 @@
+import icon from './CzsIcon.vue'
+
+const czsIcon = {
+  install: function(Vue) {
+    Vue.component('CzsIcon', icon)
+  }
+}
+
+export default czsIcon
