c3c653f26e72c434e609c413e694fb10de867dc0
Coder.HanXin <Coder.HanXin@gmail.com>
2018-01-24 17:23:01 +0800

    add filter

--- src/main.js
+++ src/main.js
@@ -6,11 +6,21 @@ import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import './style/index.styl'
 import http from './api'
+import moment from 'moment'
 
 Vue.config.productionTip = false
 Vue.use(CzsIcon)
 Vue.use(iView)
 Vue.prototype.$http = http
+
+Vue.filter('deadline', function(date) {
+  let d = moment(date, ['YYYY-MM-DD HH:mm', moment.ISO_8601])
+  if (d.hour() === 0 && d.minute() === 0 && d.second() === 0) {
+    return d.format('YYYY-MM-DD')
+  } else {
+    return date
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
