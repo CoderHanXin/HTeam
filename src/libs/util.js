f5015ff746ed6cc29cb8955781b540ef733947a8
Coder.HanXin <Coder.HanXin@gmail.com>
2018-01-24 13:23:15 +0800

    add function

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,9 +1,20 @@
 'use strict'
 
+const moment = require('moment')
+
 let util = {}
 util.title = function(title) {
   title = title || 'HTeam'
   window.document.title = title
+}
+
+util.trimDate = function(date) {
+  let d = moment(date, 'YYYY-MM-DD HH:mm')
+  if (d.hour() === 0 && d.minute() === 0 && d.second() === 0) {
+    return d.format('YYYY-MM-DD')
+  } else {
+    return date
+  }
 }
 
 export default util
