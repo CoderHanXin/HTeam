c439173625d6269c9f1b7231d3faa3aabe47b87a
Coder.HanXin <Coder.HanXin@gmail.com>
2018-01-27 19:12:43 +0800

    add constant

---
+++ src/common/constant/task_filter.js
@@ -0,0 +1,61 @@
+export const taskMyFilters = [
+  {
+    value: 100,
+    name: '全部任务'
+  },
+  // {
+  //   value: 10,
+  //   name: '看板视图'
+  // },
+  // {
+  //   value: 11,
+  //   name: '按优先级'
+  // },
+  // {
+  //   value: 12,
+  //   name: '按项目'
+  // },
+  {
+    value: 13,
+    name: '按截止日期'
+  },
+  {
+    value: 14,
+    name: '按更新时间'
+  },
+  {
+    value: 15,
+    name: '按完成时间'
+  }
+]
+
+export const taskProjectFilters = [
+  {
+    value: 100,
+    name: '全部任务'
+  },
+  // {
+  //   value: 10,
+  //   name: '看板视图'
+  // },
+  // {
+  //   value: 11,
+  //   name: '任务列表'
+  // },
+  // {
+  //   value: 12,
+  //   name: '日历视图'
+  // },
+  {
+    value: 13,
+    name: '按截止日期'
+  },
+  {
+    value: 14,
+    name: '按负责人'
+  },
+  {
+    value: 15,
+    name: '按完成时间'
+  }
+]
