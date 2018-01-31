7b8406a4cd8913b9856641662784246c27ccb0f5
Coder.HanXin <Coder.HanXin@gmail.com>
2018-01-31 15:26:53 +0800

    add api

--- src/api/url.js
+++ src/api/url.js
@@ -4,6 +4,8 @@ export default {
   user_create: '/api/user',
   user_update: '/api/user/update',
   user_delete: '/api/user/delete',
+  group_create: '/api/group',
+  group_update: '/api/group',
   org_tree: '/api/org/tree',
   task_create: '/api/task/create',
   task_update: '/api/task/update',
