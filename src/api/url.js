458ad8114a2588f473ef652caa3ac683a64b117a
Coder.HanXin <Coder.HanXin@gmail.com>
2018-02-02 13:34:02 +0800

    change api url

--- src/api/url.js
+++ src/api/url.js
@@ -2,12 +2,12 @@ export default {
   user: '/api/user',
   user_login: '/api/user/login',
   user_create: '/api/user',
-  user_update: '/api/user/update',
-  user_delete: '/api/user/delete',
+  user_update: '/api/user/:id',
+  user_delete: '/api/user/:id',
   group_create: '/api/group',
-  group_update: '/api/group',
+  group_update: '/api/group/:id',
   org_tree: '/api/org/tree',
   task_create: '/api/task/create',
-  task_update: '/api/task/update',
+  task_update: '/api/task/:id',
   task_inbox: '/api/task/inbox'
 }
