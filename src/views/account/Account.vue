f3e69fe6d7891051c27322d3b262e5a1a8c8e8c0
Coder.HanXin <Coder.HanXin@gmail.com>
2018-02-06 10:10:05 +0800

    add account

---
+++ src/views/account/Account.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <Sider class="sub-sider">
+      <div class="header">
+        <h5 class="title">个人中心</h5>
+      </div>
+      <Menu ref="teamMenu" class="menu" theme="light" width="auto" :active-name="$route.name" @on-select="handleChangeMenu">
+        <menu-item name="profile">
+            <Icon type="ios-barcode-outline" size="16"></Icon>
+            个人设置
+        </menu-item>
+        <menu-item name="password">
+            <Icon type="ios-barcode-outline" size="16"></Icon>
+            修改密码
+        </menu-item>
+      </Menu>
+    </Sider>
+    <router-view/>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: 'Account',
+  methods: {
+    handleChangeMenu(name) {
+      this.$router.push({
+        name: name
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
