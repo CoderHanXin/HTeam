9b5d5e3863d9c66b3fe42fc61959a26a4658a782
Coder.HanXin <Coder.HanXin@gmail.com>
2018-02-28 19:54:03 +0800

    chore: working on weekly stats

--- src/views/stats/StatsWeekly.vue
+++ src/views/stats/StatsWeekly.vue
@@ -3,29 +3,56 @@
     <div class="header">
       <div class="title">{{mainTitle}}</div>
       <div class="panel">
+        <DateRange @on-change="handleRangeChange"></DateRange>
       </div>
     </div>
     <Content class="content">
+      <TaskStats :all="summary.all" :done="summary.done" :expired="summary.expired"></TaskStats>
     </Content>
   </Layout>
 </template>
 
 <script>
+import statsService from '@/api/services/stats'
+import DateRange from './components/DateRange'
+import TaskStats from './components/TaskStats'
+import { mapGetters } from 'vuex'
 export default {
   name: 'StatsWeekly',
+  components: {
+    DateRange,
+    TaskStats
+  },
   data() {
     return {
-      mainTitle: '任务周报'
+      mainTitle: '任务周报',
+      summary: {
+        all: 0,
+        done: 0,
+        expired: 0
+      }
     }
   },
   computed: {
+    ...mapGetters([
+      'currentUser',
+      'currentTeam'
+    ])
   },
   created() {
     this.init()
   },
   methods: {
     init() {
+      this.getSummary()
+    },
+    handleRangeChange(start, end) {
 
+    },
+    getSummary() {
+      statsService.getSummary(this.currentTeam.id).then(res => {
+        this.summary = res.data.data
+      })
     }
   }
 }
