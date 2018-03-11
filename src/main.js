fb41006fb04008c6c1afeca47f3e65733c66adab
Coder.HanXin <Coder.HanXin@gmail.com>
2018-03-11 15:10:29 +0800

    chore: delete unused component

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from './store'
 import http from './api'
 import moment from 'moment'
 import * as filters from './common/filters'
-import CzsIcon from './views/components/czs-icon'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import 'quill/dist/quill.core.css'
@@ -13,7 +12,6 @@ import 'quill/dist/quill.snow.css'
 import './style/index.styl'
 
 Vue.config.productionTip = false
-Vue.use(CzsIcon)
 Vue.use(iView)
 Vue.prototype.$http = http
 moment.locale('zh-cn')
--- src/views/components/czs-icon/index.js
+++
@@ -1,9 +0,0 @@
-import icon from './CzsIcon.vue'
-
-const czsIcon = {
-  install: function(Vue) {
-    Vue.component('CzsIcon', icon)
-  }
-}
-
-export default czsIcon
