805ea3ff810ed9ef5d31213e05561907619925dc
Coder.HanXin <Coder.HanXin@gmail.com>
2018-03-28 16:53:32 +0800

    refactor: move allUsers to app module

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -2,12 +2,16 @@ import Cookies from 'js-cookie'
 
 const state = {
   currentUser: Cookies.getJSON('currentUser'),
-  currentTeam: Cookies.getJSON('currentTeam')
+  currentTeam: Cookies.getJSON('currentTeam'),
+  allUsers: [],
+  allGroups: []
 }
 
 const getters = {
   currentUser: state => state.currentUser,
-  currentTeam: state => state.currentTeam
+  currentTeam: state => state.currentTeam,
+  allUsers: state => state.allUsers,
+  allGroups: state => state.allGroups
 }
 
 const mutations = {
@@ -16,6 +20,12 @@ const mutations = {
   },
   setCurrentTeam(state, team) {
     state.currentTeam = team
+  },
+  setAllUsers(state, users) {
+    state.allUsers = users
+  },
+  setAllGroups(state, groups) {
+    state.allGroups = groups
   }
 }
 
@@ -23,6 +33,8 @@ const actions = {
   logout({ commit }) {
     commit('setCurrentUser', null)
     commit('setCurrentTeam', null)
+    commit('setAllUsers', [])
+    commit('setAllGroups', [])
     Cookies.remove('token')
     Cookies.remove('currentUser')
     Cookies.remove('currentTeam')
