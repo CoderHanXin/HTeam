36204a03dff17d38d7a739fe6ac0050719194d0c
Coder.HanXin <Coder.HanXin@gmail.com>
2018-04-03 21:46:28 +0800

    chore: delete proxy settings

--- config/index.js
+++ config/index.js
@@ -9,15 +9,7 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-      '/api': {
-        target: 'http://127.0.0.1:7001/api',
-        changeOrigin: true,
-        pathRewrite:{
-          '^/api': '/api'
-        }
-      }
-    },
+    proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
